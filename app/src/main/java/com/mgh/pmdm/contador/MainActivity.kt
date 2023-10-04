84a9f75874095b4876c24c88f72fe2577c89211d
raulGarAlc04 <raulgarciaalcantara7@gmail.com>
2023-10-04 09:41:18 +0200

    Nueva version, añado un comentario

--- app/src/main/java/com/mgh/pmdm/contador/MainActivity.kt
+++ app/src/main/java/com/mgh/pmdm/contador/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Comentario commit
+
         // Referencia al TextView
         val textViewContador=findViewById<TextView>(R.id.textViewContador)
 
